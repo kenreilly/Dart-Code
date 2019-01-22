3892785b6610ebf47943da6a3b69be6b55265087
Danny Tuppeny <danny@tuppeny.com>
2019-01-22 16:40:19 +0000

    Add Stagehand integration in the form of a Dart: New Project command
    Fixes #282.

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
 export const restartReasonManual = "manual";
 export const restartReasonSave = "save";
+
+export const stagehandInstallationInstructionsUrl = "https://github.com/dart-lang/stagehand#installation";
